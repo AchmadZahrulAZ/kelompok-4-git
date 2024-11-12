7fbd0105a7bcbf498dda483d97fb961382036f46
achmadzahrul01 <achmadzahrul01@gmail.com>
2024-11-12 12:03:40 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+import HomePage from './components/HomePage';
+import ContactUs from './components/ContactUs';
+import AboutUs from './components/AboutUs';
+import Profile from './components/Profile';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <LoginPage />
+      <RegisterPage />
+      <HomePage />
+      <ContactUs />
+      <AboutUs />
+      <Profile />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
