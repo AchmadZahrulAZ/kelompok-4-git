ccbc260f139f262c1a5945f3d8753dfe19b31b9d
achmadzahrul01 <achmadzahrul01@gmail.com>
2024-11-12 13:09:09 +0700

    feat(Footer, Navbar, App): Adding Content Footer and Navbar. Updating App.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <footer className="bg-dark text-white py-3">
+      <div className="container text-center">
+        <p>&copy; {new Date().getFullYear()} Kelompok 4. All rights reserved.</p>
+      </div>
+    </footer>
   );
 };
 
